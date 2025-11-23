5d29467cbed78375df8c99b332c165394554b5d2
kristina filimonova <kristina.jk7@yandex.ru>
2025-11-23 14:47:04 +0300

    обновление

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/interactive-cube-project/', 
+  base: '/interactive-cube-project/', // ОБЯЗАТЕЛЬНО с / в начале и конце
+  build: {
+    outDir: 'dist', // папка сборки
+    assetsDir: 'assets' // папка для ресурсов (по умолчанию)
+  }
 });
